673f106c839ecc3ec728f0183949d63af1205a71
Sarah Aristain <sarah@klab.com>
2012-11-13 16:01:03 +0800

    Added register function (for viewing form)

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,29 +2,40 @@
 
 class Home extends CI_Controller {
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->load->model('Users_model', 'user');
-		$this->load->model('Follow_model', 'follow');
-	}
-	
-	public function index()
-	{
-		//list all users
-		list($data['users'], $data['user_count']) = $this->user->getUsersOrderBy();
-		
-		//followers
-		list($data['user_follower'], $data['user_follower_count']) = $this->user->getUserFollowers(1);
-		
-		//following
-		list($data['user_following'], $data['user_following_count']) = $this->user->getUserFollowing(1);
-		
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Users_model', 'user');
+        $this->load->model('Follow_model', 'follow');
+    }
+
+    public function index()
+    {
+        //list all users
+        list($data['users'], $data['user_count']) = $this->user->getUsersOrderBy();
+
+        //followers
+        list($data['user_follower'], $data['user_follower_count']) = $this->user->getUserFollowers(1);
+
+        //following
+        list($data['user_following'], $data['user_following_count']) = $this->user->getUserFollowing(1);
+
         $data['header'] = $this->load->view('header', TRUE);
         $data['footer'] = $this->load->view('footer', TRUE);
-		$this->load->view('home_view', $data);
-	}
-<<<<<<< HEAD
+        $this->load->view('home_view', $data);
+    }
+
+    public function follow($following_id)
+    {
+        $this->follow->followUser(1, $following_id);
+        redirect();
+    }
+
+    public function unfollow($following_id)
+    {
+        $this->follow->unfollowUser(1, $following_id);
+        redirect();
+    }
 
     /**
      * Views registration form
@@ -33,27 +44,12 @@ class Home extends CI_Controller {
      * @param void
      * @return void
      */
-    public function register()
-    {
+    public function register(){
         $data['header'] = $this->load->view('header', TRUE);
         $data['footer'] = $this->load->view('footer', TRUE);
 
         $this->load->view('register', $data);
     }
-=======
-	
-	public function follow($following_id)
-	{
-		$this->follow->followUser(1, $following_id);
-		redirect();
-	}
-	
-	public function unfollow($following_id)
-	{
-		$this->follow->unfollowUser(1, $following_id);
-		redirect();
-	}
->>>>>>> 8741f5a3d27de6355e32bd340b78f06d4c667f3d
 }
 
 /* EOF home.php */
