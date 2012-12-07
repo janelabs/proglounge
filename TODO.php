a619a8d5dfdbc4cd5bafe3f33784a42b7a307bb8
Jo Erik San Jose <jesanjose@klab.com>
2012-12-07 17:45:58 +0800

    added TODO.php

---
+++ TODO.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * @TODO
+ * POSTS
+ * --------------------------------------
+ * 1. Delete post function (done)
+ * 2. Like function
+ * 3. Integrate share function in home page
+ * 4. Refactor codes
+ * 5. Load more posts profile (ongoing 10%)
+ * 6. Load more posts homepage
+ * 7. Modal profile view
+ * 8. Fix filterPost function (done)
+ * -------------------------------------- 
+ */
