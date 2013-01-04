6375d48e20a0f2643818d99f24fa33c1151508db
Sarah Aristain <sarah@klab.com>
2013-01-04 14:44:01 +0800

    Removed all files.

--- application/views/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="navbar navbar-static-bottom" style="margin-bottom:0px; margin-top:30px;">
-   <div class="container well" style="margin-bottom:0px;"	>
-        <button class="btn pull-right" id="top"><i class="icon-chevron-up"></i></button>
-   </div>   
-</div>
-</body>
-</html>
