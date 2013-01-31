c6208f0de5f364698d4fbdfdb2f2e4e831fba64d
Sarah Jane Aristain <sahjane26@gmail.com>
2013-01-31 11:33:07 +0800

    Added favicon :)

--- application/views/header.php
+++ application/views/header.php
@@ -12,6 +12,8 @@
     <script type="text/javascript" src="<?php echo base_url('public/js/timeago.js'); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url('public/js/highlight.js'); ?>"></script>
     <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
+    
+    <link rel="shortcut icon" href="favicon.ico" >
 </head>
 <script>
     $(document).ready(function() {
